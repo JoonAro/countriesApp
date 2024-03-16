30569c97c865f1857fbbd852c56f45c68f7ae178
joonaro <cupetus@gmail.com>
2024-03-16 17:52:19 +0200

    small fix

--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -10,15 +10,6 @@ const Register = () => {
     const [name, setName] = useState("");
     const [user, loading, error] = useAuthState(auth);
 
-    const generateID = () => {
-        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        let id = '';
-        for (let i = 0; i < 12; i++) {
-            const randomIndex = Math.floor(Math.random() * characters.length);
-            id += characters.charAt(randomIndex);
-        }
-        return id;
-    }
 
     const navigate = useNavigate();
 
